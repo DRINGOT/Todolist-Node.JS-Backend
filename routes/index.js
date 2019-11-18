b0df8f18788f12009c08d4e49ac9ee13a790b4b3
DRINGOT <donovan.ringot@hotmail.fr>
2019-11-18 16:13:02 +0100

    Creating todo Items

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ import express from "express";
 const router = express.Router();
 
 const todosController = require('../server/controllers').todos;
+const todoItemsController = require('../server/controllers').todoItems;
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -14,4 +15,7 @@ router.get('/api', (req, res) => res.status(200).send({
 
 router.post('/api/todos', todosController.create);
 
+router.get('/api/todos', todosController.list);
+
+router.post('/api/todos/:todoId/items', todoItemsController.create);
 module.exports = router;
