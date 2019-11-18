340815145d86232a6aff75baa6f9f093cacc2f55
DRINGOT <donovan.ringot@hotmail.fr>
2019-11-18 16:37:55 +0100

    Deleting todos

--- routes/index.js
+++ routes/index.js
@@ -22,4 +22,6 @@ router.post('/api/todos/:todoId/items', todoItemsController.create);
 router.get('/api/todos/:todoId', todosController.retrieve);
 
 router.put('/api/todos/:todoId', todosController.update);
+
+router.delete('/api/todos/:todoId', todosController.destroy);
 module.exports = router;
