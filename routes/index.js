a9d598b68e36ff010e366489ff00ce6eff34a09d
DRINGOT <donovan.ringot@hotmail.fr>
2019-11-18 16:33:55 +0100

    Updating a single todo

--- routes/index.js
+++ routes/index.js
@@ -20,4 +20,6 @@ router.get('/api/todos', todosController.list);
 router.post('/api/todos/:todoId/items', todoItemsController.create);
 
 router.get('/api/todos/:todoId', todosController.retrieve);
+
+router.put('/api/todos/:todoId', todosController.update);
 module.exports = router;
